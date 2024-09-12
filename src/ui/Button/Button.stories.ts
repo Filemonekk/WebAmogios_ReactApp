d4738660ebdc826a13deb76138808b6a79c9fe08
KasiaH <kah.gda@wp.pl>
2024-09-12 11:20:02 +0200

    odded new styles to Button component

--- src/ui/Button/Button.stories.ts
+++ src/ui/Button/Button.stories.ts
@@ -26,3 +26,17 @@ export const Disabled: Story = {
     disabled: true,
   },
 };
+
+export const Red: Story = {
+  args: {
+    label: 'Here!',
+    className: 'bg-red-600 hover:bg-red-500'
+  },
+};
+
+export const Green: Story = {
+  args: {
+    label: 'Here!',
+    className: 'bg-green-600 hover:bg-green-500'
+  },
+};
