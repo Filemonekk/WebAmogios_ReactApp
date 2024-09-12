50e605090abc55833fdd4171097291e10e903e75
KasiaH <kah.gda@wp.pl>
2024-09-12 10:57:04 +0200

    changed data

--- src/ui/Button/Button.tsx
+++ src/ui/Button/Button.tsx
@@ -3,8 +3,8 @@ type Props = {
     onClick: () => void
 };
 
-export const Button = ({ label, onClick }: Props) => {
+export const Button = ({ label, className, onClick }: Props) => {
     return (
-        <button onClick={onClick} className="px-4 py-1 text-sm text-white bg-blue-600 rounded-full border border-blue-900">{label}</button>
+        <button onClick={onClick} className={`px-4 py-1 text-sm text-white bg-blue-600 rounded-full border border-blue-900 ${className}`}>{label}</button>
     );
 };
