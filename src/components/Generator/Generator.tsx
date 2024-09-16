f77243ce99f36e31ef9edc943463b1d482565f76
KasiaH <kah.gda@wp.pl>
2024-09-16 11:34:26 +0200

    working with state

--- src/components/Generator/Generator.tsx
+++ src/components/Generator/Generator.tsx
@@ -1,15 +1,18 @@
+import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { Button, Text } from "../../ui";
 
 export const Generator = () => {
+    const state = useState(uuidv4());
+
     const handleClick = () => {
-        console.log('click');
-        window.location.reload();
+        //id = uuidv4();
+        state[1](uuidv4())
 }
 
     return (
         <div>
-            <Text>{uuidv4()}</Text>
+            <Text>{state[0]}</Text>
             <Button label="Refresh" onClick={handleClick}/>
         </div>
     );
