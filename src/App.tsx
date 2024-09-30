c15eff9b3871197a16561a1587c4fa8e13424b1f
KasiaH <kah.gda@wp.pl>
2024-09-30 14:37:48 +0200

    Custom input

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
 import "./App.css";
-import { Button, Text } from "./ui";
-import { Generator } from "./components/Generator";
-import { Counter } from "./components/Counter";
-import { RegistrationFormState, RegistrationFormRefs } from "./components/RegistrationForm";
+
+import {
+  RegistrationFormCustomInput
+} from "./components/RegistrationForm";
 
 function App() {
   return (
     <>
-      <Text>WItaj Amigo!</Text>
-      <Generator />
-      <Button label="Click me!" onClick={() => alert('Hey!')}></Button>
-      <Counter />
-      <RegistrationFormState />
-      <RegistrationFormRefs/>
-  
+      
+      <RegistrationFormCustomInput />
     </>
   );
 }
